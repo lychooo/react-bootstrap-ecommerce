b9ff4c9024c57aa6445428a34b47e602261facf8
Lachezar Borisov <ly.borisov@gmail.com>
2020-04-29 20:46:30 +0300

    Renamed component and folder

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import Navigation from './Components/Navigation/navigation.component';
 import Home from './Components/Home/home.component';
 import ProductPage from './Components/ProductPage/product-page.component';
-import Login from './Services/Login/login.component';
-import Register from './Services/Register/register.component';
-import Page404 from './Services/Page404/page-404.component';
-import Drones from './Components/Drones/drones.component';
+import Login from './Pages/Login/login.component';
+import Register from './Pages/Register/register.component';
+import Page404 from './Pages/Page404/page-404.component';
+import ItemsOverview from './Components/ItemsOverview/items-overview.component';
 
 class App extends React.Component {
   constructor(props) {
@@ -59,7 +59,7 @@ class App extends React.Component {
             <Navigation currentUser={currentUser} />
             <Switch>
               <Route path="/" exact component={Home} />
-              <Route path="/drones" component={Drones} />
+              <Route path="/drones" component={ItemsOverview} />
               <Route exact path="/login" render={() => currentUser ? (<Redirect to='/' />) : (<Login />)} />
               <Route exact path="/register" render={() => currentUser ? (<Redirect to='/' />) : (<Register />)} />
               <Route path="/eachine/:id" component={ProductPage} />
